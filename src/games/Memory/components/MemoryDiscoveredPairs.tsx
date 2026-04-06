308fdb14c2795deb566c043a49e1fe2561ee561f
Lucas Vogel <lucas@deepjudge.ai>
2026-04-06 22:32:08 +0200

    distribute around viewport

--- src/games/Memory/components/MemoryDiscoveredPairs.tsx
+++ src/games/Memory/components/MemoryDiscoveredPairs.tsx
@@ -1,70 +1,83 @@
 import { For } from "solid-js";
 import { useMemoryStore } from "../store";
-import { idToMemoryPair, memoryPairs } from "~/data/memory";
-import { createCards } from "../utils/cards";
+import { idToMemoryPair, MemoryPairId, memoryPairs } from "~/data/memory";
+import { Card, createCards } from "../utils/cards";
 import { idToIngredient } from "~/data/ingredients";
 import { MemoryCard } from "./MemoryCard";
 import { cn } from "~/utils/cn";
 
+type PairEdges = [
+  topLeft: Card[][],
+  top: Card[][],
+  topRight: Card[][],
+  right: Card[][],
+  bottomRight: Card[][],
+  bottom: Card[][],
+  bottomLeft: Card[][],
+  left: Card[][],
+];
+
 export function MemoryDiscoveredPairs() {
   const [store] = useMemoryStore();
-  return (
-    <For each={store.discoveredPairs}>
-      {(pairId, pairIdx) => {
+  const pairGroups = () =>
+    store.discoveredPairs.reduce(
+      (acc, pairId) => {
         const pair = idToMemoryPair[pairId];
         const ingredients = pair.ingredients.map((id) => idToIngredient[id]);
         const cards = createCards(ingredients);
-        return (
-          <div class="absolute-full overflow-clip">
-            <div
-              class={cn(
-                "absolute left-1/2 top-1/2 -translate-1/2 size-[100vmax] max-w-screen max-h-screen",
-                "flex justify-center items-center ",
-              )}
-            >
-              <div
-                class="relative"
-                style={{
-                  "--radius": "45vmax",
-                  "--total": `${memoryPairs.length}`,
-                  width: `calc(var(--radius) * 2)`,
-                  height: `calc(var(--radius) * 2)`,
-                }}
-              >
-                <div
-                  class="absolute left-1/2 top-1/2 size-48"
-                  style={{
-                    "--i": `${pairIdx()}`,
-                    "--angle": `calc(360deg / var(--total) * var(--i))`,
-                    transform: `rotate(var(--angle)) translateX(var(--radius)) rotate(calc(-1 * var(--angle)))`,
-                  }}
-                >
-                  <div class="size-48 relative">
-                    <For each={cards}>
-                      {(card, cardIdx) => (
-                        <MemoryCard
-                          {...card.ingredient}
-                          colorClass={card.colorClass}
-                          isRevealed={() => true}
-                          pairIsDiscovered={() => true}
-                          onToggleReveal={() => {}}
-                          rotateLeft={card.rotateLeft}
-                          class={cn(
-                            "size-48 absolute top-1/2 left-1/2 -translate-1/2",
-                            cardIdx() === 0
-                              ? "translate-x-[calc(-50%-4rem)]"
-                              : "translate-x-[calc(-50%+4rem)] translate-y-[calc(-50%+4rem)]",
-                          )}
-                        />
-                      )}
-                    </For>
-                  </div>
+        const originalIndex = memoryPairs.findIndex(({ id }) => id === pairId);
+        const edgeIndex = originalIndex % acc.length;
+        acc[edgeIndex] = [...acc[edgeIndex], cards];
+        return acc;
+      },
+      [[], [], [], [], [], [], [], []] as PairEdges,
+    );
+  return (
+    <div class="absolute-full overflow-clip">
+      <For each={pairGroups()}>
+        {(pairGroup, pairGroupIndex) => (
+          <div
+            class={cn(
+              "absolute",
+              [
+                "top-16 left-16 -translate-1/2",
+                "top-16 left-1/2 -translate-1/2",
+                "top-16 right-16 translate-x-1/2 -translate-y-1/2",
+                "top-1/2 -translate-y-1/2 right-16 translate-x-1/2",
+                "bottom-16 right-16 translate-1/2",
+                "bottom-16 left-1/2 -translate-x-1/2 translate-y-1/2",
+                "bottom-16 left-16 -translate-x-1/2 translate-y-1/2",
+                "left-16 top-1/2 -translate-1/2",
+              ][pairGroupIndex()],
+            )}
+          >
+            <For each={pairGroup}>
+              {(cards) => (
+                <div class="size-32 relative">
+                  <For each={cards}>
+                    {(card, cardIdx) => (
+                      <MemoryCard
+                        {...card.ingredient}
+                        colorClass={card.colorClass}
+                        isRevealed={() => true}
+                        pairIsDiscovered={() => true}
+                        onToggleReveal={() => {}}
+                        rotateLeft={card.rotateLeft}
+                        class={cn(
+                          "size-32 absolute top-1/2 left-1/2 -translate-1/2",
+                          cardIdx() === 0
+                            ? "translate-x-[calc(-50%-2rem)]"
+                            : "translate-x-[calc(-50%+2rem)] translate-y-[calc(-50%+2rem)]",
+                        )}
+                      />
+                    )}
+                  </For>
                 </div>
-              </div>
-            </div>
+              )}
+            </For>
           </div>
-        );
-      }}
-    </For>
+        )}
+      </For>
+    </div>
   );
 }
